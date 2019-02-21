cf63fb2a27b162f2fcc7e8b00e9ed54c234e895f
Brajam <47007305+Brajam@users.noreply.github.com>
2019-02-20 20:22:36 -0600

    Create hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,15 @@
+##Import flask
+from flask import Flask
+
+##definition of the REST API
+app = Flask(__name__)
+@app.route(’/hello’, methods=[’GET’])
+
+##Function hello
+def hello_world():
+return "Hello, world!"
+
+
+if __name__ == "__main__":
+
+app.run(host=’0.0.0.0’, port=8080)

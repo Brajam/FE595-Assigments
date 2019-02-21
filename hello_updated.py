57843137a750aaea24e875449802d8e69cec7a79
Brajam <47007305+Brajam@users.noreply.github.com>
2019-02-21 16:56:12 -0600

    Update hello_updated.py
    simplify function

--- hello_updated.py
+++ hello_updated.py
@@ -1,17 +1,18 @@
 ##Import flask
 from flask import Flask
 from flask import render_template
-import requests
+#import requests
 
 
 ##definition of the REST API
 app = Flask(__name__)
 @app.route('/hello_updated', methods=['GET'])
 
-##Function
+#Function
 def hello_updated():
+        return render_template('index.html')
 #        get_request = requests.get('https://theyfightcrime.org/')
-        return render_template('index.html', title='Hola Mundo')
+#        return render_template('index.html', title='Hola Mundo')
 
 
 if __name__ == "__main__":

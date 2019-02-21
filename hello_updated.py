77acdc60e64742d5e3682799263b68b982567844
Brajam <47007305+Brajam@users.noreply.github.com>
2019-02-21 16:38:36 -0600

    Created test
    Created hello updated that it will request fight crime and will render a html

---
+++ hello_updated.py
@@ -0,0 +1,18 @@
+##Import flask
+from flask import Flask
+from flask import render_template
+import request
+
+
+##definition of the REST API
+app = Flask(__name__)
+@app.route('/hello_updated', methods=['GET'])
+
+##Function
+def hello_updated():
+        get_request = requests.get('https://theyfightcrime.org/')
+        return render_template('index.html',title='Hola Mundo')
+
+
+if __name__ == "__main__":
+        app.run(host='0.0.0.0', port=8080)
